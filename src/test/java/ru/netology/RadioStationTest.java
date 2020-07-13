c9a636f14bf8df5b946aadf034723ee9ecf51cc6
Mikhail Usachev <63136175+MikhailUsachev-Piter@users.noreply.github.com>
2020-07-13 09:29:15 +0300

    Update RadioStationTest.java

--- src/test/java/ru/netology/RadioStationTest.java
+++ src/test/java/ru/netology/RadioStationTest.java
@@ -8,33 +8,33 @@ class RadioStationTest {
 
     @Test
     void nextStatin() {
-        Radio radio = new Radio();
-        radio.setCurrentStation(8);
+        Radio radio = new Radio(8, 9, 0, 5, 100, 0);
         radio.nextStatin();
+
         assertEquals(9, radio.getCurrentStation());
     }
 
     @Test
     void currentStatinEqualMaxStation() {
-        Radio radio = new Radio();
-        radio.setCurrentStation(9);
+        Radio radio = new Radio(9, 5);
         radio.nextStatin();
+
         assertEquals(radio.getMinStation(), radio.getCurrentStation());
     }
 
     @Test
     void prevStation() {
-        Radio radio = new Radio();
-        radio.setCurrentStation(1);
+        Radio radio = new Radio(1, 1);
         radio.prevStation();
+
         assertEquals(0, radio.getCurrentStation());
     }
 
     @Test
     void currentStationEqualMinStation() {
-        Radio radio = new Radio();
-        radio.setCurrentStation(0);
+        Radio radio = new Radio(0, 5);
         radio.prevStation();
+
         assertEquals(radio.getMaxStation(), radio.getCurrentStation());
     }
 }
